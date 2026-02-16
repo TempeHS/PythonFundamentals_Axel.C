e3b4b7c7f9dfe7160153a8b5f5df80100f5b6624
Axel C <159206642+TheSpiciestRamen@users.noreply.github.com>
2026-02-16 22:21:19 +0000

    Upload completed extensions.py

---
+++ 1-Conditionals/extensions/extensions.py
@@ -0,0 +1,14 @@
+userInput = input("Please input the name of a file: ")
+match userInput.lower().split(".")[-1]:
+    case "gif":
+        print("image/gif")
+    case "jpg" | "jpeg":
+        print("image/jpeg")
+    case "png":
+        print("image/png")
+    case "pdf":
+        print("application/pdf")
+    case "txt":
+        print("text/plain")
+    case "zip":
+        print("application/zip")
